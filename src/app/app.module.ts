468096cb2580ccf33b33efd667840904bcef2c0b
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-15 09:16:55 -0300

    add effects of items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/effects/items.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { ActionsSubject, Action } from "@ngrx/store";
+import { AngularFireDatabase } from "angularfire2/database";
+
+import { Effect, ofType } from "@ngrx/effects";
+import { Observable, combineLatest, of } from "rxjs";
+import { map, mergeMap, take, catchError } from "rxjs/operators";
+import { ItemActionTypes, LoadSucess, LoadFail, Load } from "../actions/items";
+import { Item } from "../models/item";
+
+@Injectable()
+export class ItemsEffects {
+  constructor(
+    private actions$: ActionsSubject,
+    private db: AngularFireDatabase
+  ) {}
+
+  @Effect() loadItems$: Observable<Action> = this.actions$.pipe(
+    ofType(ItemActionTypes.Load),
+    map((action: Load) => action.payload),
+    mergeMap((ids: number[]) =>
+      combineLatest(
+        ids.map(id =>
+          this.db
+            .object("/v0/item" + id)
+            .valueChanges()
+            .pipe(take(1))
+        )
+      ).pipe(
+        map((items: Item[]) => new LoadSucess(items)),
+        catchError(error => of(new LoadFail(error)))
+      )
+    )
+  );
+}
