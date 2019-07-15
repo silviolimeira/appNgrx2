dfdc352eea10dbec5906339857a78e1a62f2c866
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-15 08:25:31 -0300

    Add actions of the feature items at actions/items.ts

---
+++ src/app/actions/items.ts
@@ -0,0 +1,30 @@
+// The action Load means starting the loading of items;
+// LoadSuccess means items are loaded successfully;
+// LoadFail means items have failed to load.
+// Actions must have a property type to specify their types
+
+import { Action } from "@ngrx/store";
+import { Item } from "../models/item";
+
+export enum ItemActionTypes {
+  Load = "[Items] Load",
+  LoadSuccess = "[Items] Load Success",
+  LoadFail = "[Items] Load Fail"
+}
+
+export class Load implements Action {
+  readonly type = ItemActionTypes.Load;
+  constructor(public payload: number[]) {}
+}
+
+export class LoadSucess implements Action {
+  readonly type = ItemActionTypes.LoadFail;
+  constructor(public payload: any) {}
+}
+
+export class LoadFail implements Action {
+  readonly type = ItemActionTypes.LoadFail;
+  constructor(public payload: any) {}
+}
+
+export type ItemActions = Load | LoadSucess | LoadFail;
