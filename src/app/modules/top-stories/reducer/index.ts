bafc11175f965e62cad0b366cb2a6fa3c9b879d7
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-17 09:15:41 -0300

    add selectors for components

--- src/app/modules/top-stories/reducer/index.ts
+++ src/app/modules/top-stories/reducer/index.ts
@@ -2,6 +2,7 @@ import * as fromRoot from "../../../reducers/items";
 import * as fromTopStories from "../reducer/top-stories";
 import * as fromPagination from "./pagination";
 import * as fromItems from "../../../reducers/items";
+import { getItemEntities, getItemsState } from "../../../reducers/items";
 
 import {
   ActionReducerMap,
@@ -24,4 +25,60 @@ export const reducers: ActionReducerMap<TopStoriesState> = {
 };
 
 //Selectors for components
+export const getTopStoriesState = createFeatureSelector<TopStoriesState>(
+  "topStories"
+);
 
+export const getPaginationState = createSelector(
+  getTopStoriesState,
+  state => state.pagination
+);
+
+export const getStoriesState = createSelector(
+  getTopStoriesState,
+  state => state.stories
+);
+
+export const getStoryIds = createSelector(
+  getStoriesState,
+  fromTopStories.getIds
+);
+
+export const getDisplayItems = createSelector(
+  getStoryIds,
+  getItemEntities,
+  getPaginationState,
+  (ids, entities, pagination) => {
+    return {
+      results: ids
+        .slice(0, pagination.offset + pagination.limit)
+        .map(id => entities[id])
+    };
+  }
+);
+
+export const isItemsLoading = createSelector(
+  getItemsState,
+  fromItems.getLoading
+);
+
+export const getItemsError = createSelector(
+  getItemsState,
+  fromItems.getError
+);
+
+export const isTopStoriesLoading = createSelector(
+  getStoriesState,
+  fromTopStories.getLoading
+);
+
+export const getTopStoriesError = createSelector(
+  getStoriesState,
+  fromTopStories.getError
+);
+
+export const getError = createSelector(
+  getTopStoriesError,
+  getItemsError,
+  (e1, e2) => e1 || e2
+);
