41722c5ad6db8cde50c8883a94d7d95482d7c94c
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-17 08:34:16 -0300

    compose state module, add index.tx with State and reducers of top stories

---
+++ src/app/modules/top-stories/reducer/index.ts
@@ -0,0 +1,24 @@
+import * as fromRoot from "../../../reducers/items";
+import * as fromTopStories from "../reducer/top-stories";
+import * as fromPagination from "./pagination";
+import * as fromItems from "../../../reducers/items";
+
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector
+} from "@ngrx/store";
+
+export interface TopStoriesState {
+  stories: fromTopStories.State;
+  pagination: fromPagination.State;
+}
+
+export interface State extends fromRoot.State {
+  topStories: TopStoriesState;
+}
+
+export const reducers: ActionReducerMap<TopStoriesState> = {
+  stories: fromTopStories.reducer,
+  pagination: fromPagination.reducer
+};
