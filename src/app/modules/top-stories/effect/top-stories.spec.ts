3cb1db3802aec0bdc9a3de2f638f6e9ca53c2983
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-17 08:03:26 -0300

    add Effects of top stories

---
+++ src/app/modules/top-stories/effect/top-stories.spec.ts
@@ -0,0 +1,7 @@
+import { TopStories } from './top-stories';
+
+describe('TopStories', () => {
+  it('should create an instance', () => {
+    expect(new TopStories()).toBeTruthy();
+  });
+});
---
+++ src/app/modules/top-stories/effect/top-stories.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Action, Store } from "@ngrx/store";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { TopStoriesActionTypes } from "../action/top-stories";
+import {
+  catchError,
+  map,
+  mergeMap,
+  switchMap,
+  take,
+  withLatestFrom
+} from "rxjs/operators";
+import { AngularFireDatabase } from "@angular/fire/database";
+import * as fromTopStories from "../reducer";
+import { pageSize } from "../reducer/pagination";
+import * as itemActions from "../actions/items";
+import * as topStoriesActions from "../actions/top-stories";
+
+@Injectable()
+export class TopStories {
+  constructor(
+    private actions$,
+    private store: Store<fromTopStories.State>,
+    private db: AngularFireDatabase
+  ) {}
+
+  @Effect()
+  loadTopStories$: Observable<Action> = this.actions$.pipe(
+    ofType(TopStoriesActionTypes.Refresh),
+    switchMap(() =>
+      this.db
+        .list("/v0/topstories")
+        .valueChanges()
+        .pipe(
+          take(1),
+          mergeMap((ids: number[]) =>
+            of<Action>(
+              new topStoriesActions.LoadSuccess(ids),
+              new itemActions.Load(ids.slice(0, pageSize))
+            )
+          ),
+          catchError(error => of(new topStoriesActions.LoadFail(error)))
+        )
+    )
+  );
+
+  @Effect()
+  loadMore$: Observable<Action> = this.actions$.pipe(
+    ofType(TopStoriesActionTypes.LoadMore),
+    withLatestFrom(this.store),
+    map(([action, state]) => {
+      const {
+        pagination: { offset, limit },
+        stories: { ids }
+      } = state.topStories;
+      return new itemActions.Load(ids.slice(offset, offset + limit));
+    })
+  );
+
+  
+}
