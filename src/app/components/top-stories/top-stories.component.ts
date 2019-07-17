b787d52669e0b8930293d3b7dc11320a0a73b291
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-17 12:01:47 -0300

    add TopStoriesComponent using NgRx

---
+++ src/app/components/top-stories/top-stories.component.ts
@@ -0,0 +1,141 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit
+} from "@angular/core";
+import { from, Observable, Subscription } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { Items } from "../../models/items";
+import { LoadingController, ToastController } from "@ionic/angular";
+import * as fromTopStories from "../../modules/top-stories/reducer";
+import * as topStoriesActions from "../../modules/top-stories/action";
+import { filter, concatMap } from "rxjs/operators";
+
+@Component({
+  selector: "app-top-stories",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./top-stories.component.html",
+  styleUrls: ["./top-stories.component.scss"]
+})
+export class TopStoriesComponent implements OnInit, OnDestroy {
+  items$: Observable<any>;
+
+  private itemsLoading$: Observable<boolean>;
+  private idsLoading$: Observable<boolean>;
+  private error$: Observable<any>;
+  private infiniteScrollComponent: any;
+  private refresherComponent: any;
+  private loading: HTMLIonLoadingElement;
+  private subscriptions: Subscription[];
+
+  constructor(
+    private store: Store<fromTopStories.State>,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
+  ) {
+    this.items$ = store.pipe(select(fromTopStories.getDisplayItems));
+    this.itemsLoading$ = store.pipe(select(fromTopStories.isItemsLoading));
+    this.idsLoading$ = store.pipe(select(fromTopStories.isTopStoriesLoading));
+    this.error$ = store.pipe(
+      select(fromTopStories.getError),
+      filter(error => error != null)
+    );
+    this.subscriptions = [];
+  }
+
+  ngOnInit() {
+    this.subscriptions.push(
+      this.itemsLoading$.subscribe(loding => {
+        if (!this.loading) {
+          this.notifyScrollComplete();
+        }
+      })
+    );
+
+    this.subscriptions.push(
+      this.idsLoading$
+        .pipe(
+          concatMap(loading => {
+            return loading
+              ? from(this.showLoading())
+              : from(this.hideLoading());
+          })
+        )
+        .subscribe()
+    );
+    this.subscriptions.push(
+      this.error$
+        .pipe(concatMap(error => from(this.showError(error))))
+        .subscribe()
+    );
+    this.doLoad(true);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  load(event) {
+    this.infiniteScrollComponent = event.target;
+    this.doLoad(false);
+  }
+
+  refresh(event) {
+    this.refresherComponent = event.target;
+    this.doLoad(true);
+  }
+
+  doLoad(refresh: boolean) {
+    if (refresh) {
+      this.store.dispatch(new topStoriesActions.refresh());
+    } else {
+      this.store.dispatch(new topStoriesActions.LoadMore());
+    }
+  }
+
+  private notifyScrollComplete(): void {
+    if (this.infiniteScrollComponent) {
+      this.infiniteScrollComponent.complete();
+    }
+  }
+
+  private notifyRefreshComplete(): void {
+    if (this.refresherComponent) {
+      this.refresherComponent.complete();
+    }
+  }
+
+  private showLoading(): Promise<void> {
+    return this.hideLoading().then(() => {
+      return this.loadingCtrl
+        .create({
+          message: "Loading",
+          spinner: "crescent",
+          duration: 2000
+        })
+        .then(loading => {
+          this.loading = loading;
+          return this.loading.present();
+        });
+    });
+  }
+
+  private hideLoading(): Promise<void> {
+    if (this.loading) {
+      this.notifyRefreshComplete();
+      return this.loading.dismiss().then(() => null);
+    }
+    return Promise.resolve();
+  }
+
+  private showError(error: any): Promise<void> {
+    return this.toastCtrl
+      .create({
+        message: `An error ocurred: ${error}`,
+        duration: 3000,
+        showCloseButton: true
+      })
+      .then(toast => toast.present());
+  }
+}
